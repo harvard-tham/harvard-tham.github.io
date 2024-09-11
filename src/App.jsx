d316ce5ecd3831f302a296c000d0940f1e43cf6f
Harvard Tham <64189277+harvard-tham@users.noreply.github.com>
2024-09-11 02:22:21 +0100

    add WIP page

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <h1>Harvard&apos;s WIP Portfolio Website</h1>
+      <p>Experimenting with React on Github pages</p>
+      <p>
+        Check out my{" "}
+        <a href="https://www.linkedin.com/in/harvard-tham/">LinkedIn</a> in the
+        meantime
+      </p>
+    </>
+  );
+}
+
+export default App;
